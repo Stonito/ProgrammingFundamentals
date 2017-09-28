1c3abaa0105cb892ff83ba88f7b40cc7b3486eeb
Stonito <stonito97@gmail.com>
2017-09-28 19:37:09 +0300

    C# Intro and Basic Syntax - Lab.

---
+++ 01. C# Intro and Basic Syntax/Lab - Intro and Basic Syntax/02. Add Two Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Add_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a} + {b} = {a+b}");
+        }
+    }
+}
---
+++ 01. C# Intro and Basic Syntax/Lab - Intro and Basic Syntax/03. Employee Data/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Employee_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int iD = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {iD:D8}");
+            Console.WriteLine($"Salary: {salary:F2}");
+        }
+    }
+}
